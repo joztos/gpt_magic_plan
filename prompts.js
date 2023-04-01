41ef021529dc1653c4386d2cd553cdcafa08c392
Your Name <you@example.com>
2023-04-01 16:58:43 -0600

    Primer commit

--- prompts.js
+++ prompts.js
@@ -1,36 +1,35 @@
-export const getStylePrompt = (grade) => {
+export const getLessonPlanPrompt = (grade) => {
   if (grade === "Primero de Primaria") {
-    return "Escribe una historia corta utilizando palabras sencillas y oraciones cortas. Incluye un personaje, un lugar y una acción.";
+    return "Planea una lección adecuada para estudiantes de primero de primaria. Asegúrate de utilizar instrucciones sencillas, actividades de reconocimiento y ejemplos prácticos.";
   } else if (grade === "Segundo de Primaria") {
-    return "Escribe una historia que tenga al menos dos personajes, una situación problemática y una solución. Utiliza oraciones simples y vocabulario adecuado para tu nivel.";
+    return "Diseña una lección para estudiantes de segundo de primaria. Incluye ejemplos y actividades prácticas para reforzar el aprendizaje y utiliza un lenguaje y conceptos apropiados para su nivel.";
   } else if (grade === "Tercero de Primaria") {
-    return "Crea una historia utilizando descripciones detalladas de los personajes y el entorno. Incluye un conflicto y su resolución, y asegúrate de utilizar oraciones más complejas.";
+    return "Crea una lección dirigida a estudiantes de tercero de primaria. Asegúrate de incluir descripciones detalladas, actividades de observación y registro, y utilizar un lenguaje adecuado a su nivel.";
   } else if (grade === "Cuarto de Primaria") {
-    return "Redacta un informe breve sobre un tema de interés personal o un tema que estés estudiando en la escuela. Asegúrate de incluir hechos e información relevante.";
+    return "Elabora una lección para estudiantes de cuarto de primaria. Incluye ejemplos, actividades prácticas y asegúrate de adaptar el lenguaje y los conceptos a su nivel de comprensión.";
   } else if (grade === "Quinto de Primaria") {
-    return "Escribe un texto argumentativo sobre un tema de actualidad o relevante para ti. Asegúrate de presentar una postura clara, argumentos sólidos y ejemplos para respaldar tu opinión.";
+    return "Desarrolla una lección para estudiantes de quinto de primaria. Asegúrate de presentar ideas claras, argumentos sólidos y ejemplos para respaldar el tema de la lección.";
   } else if (grade === "Sexto de Primaria") {
-    return "Redacta un ensayo que compare y contraste dos temas relacionados. Asegúrate de incluir una introducción, desarrollo de argumentos y una conclusión sólida.";
+    return "Crea una lección para estudiantes de sexto de primaria. Asegúrate de incluir una introducción, desarrollo de argumentos y actividades prácticas para reforzar el aprendizaje.";
   }
 };
 
-
-
-export const promptCreateNewSection = (pageName, currentSections) => {
+export const promptCreateNewSection = (lessonName, currentSections) => {
   let currentSectionsNames = currentSections.map((section) => section.header);
-  return `Here are the sections names created for the page ${pageName}:\n${currentSectionsNames}. Return a new section with a description. Result is in a json format like this: \{"header": "Event A ", "description": "This event happens/describe ..."}\}`;
-};
-export const promptSections = (pageName, style) => {
-  return `Can you return  7 sections of a planning class related to this subject: '${pageName}'
-  ${getStylePrompt(
-    style
-  )}.In a json format like this, in the order of appearance in the page: [\{{"header": "Event A ", "description": "This event happens/describe ..."}}, ...]`;
+  return `Estos son los nombres de las secciones creadas para la lección ${lessonName}:\n${currentSectionsNames}. Devuelve una nueva sección con una descripción. El resultado está en un formato JSON como este: {"header": "Actividad A", "description": "Esta actividad implica..."}\}`;
 };
 
-export const promptContinue = (pageName, currentSection, style) => {
-  return `This is the section named "${
+export const promptSections = (lessonName, grade) => {
+  return `Genera las secciones de la planeacion de clase relacionadas con este tema: '${lessonName}'
+  ${getLessonPlanPrompt(
+    grade
+  )}? En un formato JSON como este, en el orden de aparición en la página: [{"header": "Actividad A", "description": "Esta actividad implica..."}, ...]`;
+};
+
+export const promptContinue = (lessonName, currentSection, grade) => {
+  return `Esta es la sección llamada "${
     currentSection.header
-  }" for the page ${pageName}:\n${
+  }" para la seccion ${lessonName}:\n${
     currentSection.description
-  }.\n.regenerate the section with 1 more paragraph ${getStylePrompt(style)}:\n`;
+  }.\n.Continúa la sección con 1 párrafo adicional ${getLessonPlanPrompt(grade)}:\n`;
 };
